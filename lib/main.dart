0dc30157b50f61c8272be3fc6a9a3aba852213d5
hjlee <irr2000@paran.com>
2023-09-13 15:54:30 +0900

    Future Builder

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:webtoon_app/screens/home_screen.dart';
-import 'package:webtoon_app/services/api_service.dart';
 
 import 'services/customHttp.dart';
 
@@ -16,7 +15,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomeScreen(),
     );
   }
