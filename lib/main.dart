f331d5398848166a8f0d9a1b7687aa75e3890fb8
hjlee <irr2000@paran.com>
2023-09-13 15:40:54 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webtoon_app/screens/home_screen.dart';
+import 'package:webtoon_app/services/api_service.dart';
+
+import 'services/customHttp.dart';
+
+void main() {
+  HttpOverrides.global = CustomHttp();
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
